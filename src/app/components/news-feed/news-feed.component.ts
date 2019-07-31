b305527c34667e38bba85d0d6cdbeb9777e2dd2a
Pavel Schoffer <xschof00@gmail.com>
2019-07-31 15:33:01 +0200

    fetching source changes

--- src/app/components/news-feed/news-feed.component.ts
+++ src/app/components/news-feed/news-feed.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-news-feed',
-  templateUrl: './news-feed.component.html',
-  styleUrls: ['./news-feed.component.css']
+  selector: "app-news-feed",
+  templateUrl: "./news-feed.component.html",
+  styleUrls: ["./news-feed.component.css"]
 })
 export class NewsFeedComponent implements OnInit {
-
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.route.params.subscribe(params => this.updateSource(params.source));
   }
 
+  updateSource = (source: string) => {
+    console.log(source);
+  };
 }
